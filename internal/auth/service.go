6f95e671bb826c29c3698916f87efad0e9deeacf
sxd0 <fancityfun@gmail.com>
2025-07-13 09:32:47 +0500

    feat: add bcrypt for hashed_password

--- internal/auth/service.go
+++ internal/auth/service.go
@@ -3,6 +3,8 @@ package auth
 import (
 	"errors"
 	"go/test-http/internal/user"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type AuthService struct {
@@ -20,14 +22,17 @@ func (service *AuthService) Register(email, password, name string) (string, erro
 	if existedUser != nil {
 		return "", errors.New(ErrUserExists)
 	}
-
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
 	user := &user.User{
 		Email:    email,
-		Password: "",
+		Password: string(hashedPassword),
 		Name:     name,
 	}
 
-	_, err := service.UserRepository.Create(user)
+	_, err = service.UserRepository.Create(user)
 	if err != nil {
 		return "", err
 	}
