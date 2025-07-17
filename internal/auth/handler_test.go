7f608a409e48627f82b2112dfb5370f26f7f076a
sxd0 <fancityfun@gmail.com>
2025-07-17 14:09:49 +0500

    fix: refactoring handler_test.go

--- internal/auth/handler_test.go
+++ internal/auth/handler_test.go
@@ -1,10 +1,14 @@
 package auth_test
 
 import (
+	"bytes"
+	"encoding/json"
 	"go/test-http/configs"
 	"go/test-http/internal/auth"
 	"go/test-http/internal/user"
 	"go/test-http/pkg/db"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
@@ -12,18 +16,16 @@ import (
 	"gorm.io/gorm"
 )
 
-func TestLoginSuccess(t *testing.T) {
+func bootstrap() (*auth.AuthHandler, sqlmock.Sqlmock, error) {
 	database, mock, err := sqlmock.New()
 	if err != nil {
-		t.Fatal("Failed init mock db")
-		return
+		return nil, nil, err
 	}
 	gormDb, err := gorm.Open(postgres.New(postgres.Config{
 		Conn: database,
 	}))
 	if err != nil {
-		t.Fatal("Failed init gorm")
-		return
+		return nil, nil, err
 	}
 	userRepo := user.NewUserRepository(&db.Db{
 		DB: gormDb,
@@ -35,5 +37,26 @@ func TestLoginSuccess(t *testing.T) {
 			},
 		},
 		AuthService: auth.NewAuthService(userRepo),
+	}
+	return &handler, mock, nil
+}
+
+func TestLoginSuccess(t *testing.T) {
+	handler, _, err := bootstrap()
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	data, _ := json.Marshal(&auth.LoginRequest{
+		Email: "user@example.com",
+		Password: "123",
+	})
+	reader := bytes.NewReader(data)
+
+	w := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodPost, "/auth/login", reader)
+	handler.Login()(w, req)
+	if w.Code != http.StatusOK {
+		t.Errorf("got %d, expected %d", w.Code, 200)
 	}
 }
