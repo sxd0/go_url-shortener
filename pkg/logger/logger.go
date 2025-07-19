55c4e5f8fb9b1ff78fd5e7bef109427ae6d41d84
sxd0 <fancityfun@gmail.com>
2025-07-19 11:41:27 +0500

    fix: zap debug logging level

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -7,8 +7,10 @@ import (
 var Log *zap.Logger
 
 func InitLogger() {
+	cfg := zap.NewProductionConfig()
+	cfg.Level = zap.NewAtomicLevelAt(zap.DebugLevel)
 	var err error
-	Log, err = zap.NewProduction()
+	Log, err = cfg.Build()
 	if err != nil {
 		panic(err)
 	}
