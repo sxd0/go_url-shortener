d2f2c9101a76fe49e213aa52639d39bff5f4b944
sxd0 <fancityfun@gmail.com>
2025-07-23 16:01:28 +0500

    feat{gateway): creating middleware(jwt)

---
+++ internal/gateway/middleware/jwt.go
@@ -0,0 +1,35 @@
+package middleware
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	"github.com/sxd0/go_url-shortener/internal/gateway/jwt"
+)
+
+type ContextKey string
+
+const UserIDKey ContextKey = "x-user-id"
+
+func JWTMiddleware(verifier *jwt.Verifier) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			authHeader := r.Header.Get("Authorization")
+			if !strings.HasPrefix(authHeader, "Bearer ") {
+				http.Error(w, "unauthorized", http.StatusUnauthorized)
+				return
+			}
+
+			token := strings.TrimPrefix(authHeader, "Bearer ")
+			userID, err := verifier.ParseToken(token)
+			if err != nil {
+				http.Error(w, "unauthorized", http.StatusUnauthorized)
+				return
+			}
+
+			ctx := context.WithValue(r.Context(), UserIDKey, userID)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
+}
